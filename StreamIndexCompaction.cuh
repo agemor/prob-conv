9190b1b055f621826ef0ea330b298bc02f931cc7
Hyunjun Kim <hyunjun.leo.kim@gmail.com>
2021-04-27 16:23:01 +0900

    Update

---
+++ StreamIndexCompaction.cuh
@@ -0,0 +1,126 @@
+
+/*
+ * cuCompactor.h
+ *
+ *  Created on: 21/mag/2015
+ *      Author: knotman
+ */
+
+#ifndef CUCOMPACTOR_H_
+#define CUCOMPACTOR_H_
+
+#include <thrust/scan.h>
+#include <thrust/device_vector.h>
+
+
+#define warpSize (32)
+#define FULL_MASK 0xffffffff
+
+__host__ __device__ int divup(int x, int y) { return x / y + (x % y ? 1 : 0); }
+
+__device__ __inline__ int pow2i(int e) {
+    return 1 << e;
+}
+
+
+__global__ void computeBlockCounts(const bool *d_input, int length, int *d_BlockCounts) {
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    if (idx < length) {
+        int pred = d_input[idx];
+        int BC = __syncthreads_count(pred);
+
+        if (threadIdx.x == 0) {
+            d_BlockCounts[blockIdx.x] = BC; // BC will contain the number of valid elements in all threads of this thread block
+        }
+    }
+}
+
+
+__global__ void compactK(bool *d_input, int length, int *d_output, int *d_BlocksOffset) {
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    extern __shared__ int warpTotals[];
+    if (idx < length) {
+        int pred = d_input[idx];
+        int w_i = threadIdx.x / warpSize; //warp index
+        int w_l = idx % warpSize;//thread index within a warp
+
+        // compute exclusive prefix sum based on predicate validity to get output offset for thread in warp
+        int t_m = FULL_MASK >> (warpSize - w_l); //thread mask
+
+        int b = __ballot_sync(FULL_MASK, pred) & t_m;
+
+        int t_u = __popc(
+                b); // popc count the number of bit one. simply count the number predicated true BEFORE MY INDEX
+
+        // last thread in warp computes total valid counts for the warp
+        if (w_l == warpSize - 1) {
+            warpTotals[w_i] = t_u + pred;
+        }
+
+        // need all warps in thread block to fill in warpTotals before proceeding
+        __syncthreads();
+
+        // first numWarps threads in first warp compute exclusive prefix sum to get output offset for each warp in thread block
+        int numWarps = blockDim.x / warpSize;
+        unsigned int numWarpsMask = FULL_MASK >> (warpSize - numWarps);
+        if (w_i == 0 && w_l < numWarps) {
+            int w_i_u = 0;
+            for (int j = 0; j <=
+                            5; j++) { // must include j=5 in loop in case any elements of warpTotals are identically equal to 32
+
+                int b_j = __ballot_sync(numWarpsMask, warpTotals[w_l] & pow2i(j));
+
+                w_i_u += (__popc(b_j & t_m)) << j;
+                //printf("indice %i t_m=%i,j=%i,b_j=%i,w_i_u=%i\n",w_l,t_m,j,b_j,w_i_u);
+            }
+            warpTotals[w_l] = w_i_u;
+        }
+
+        // need all warps in thread block to wait until prefix sum is calculated in warpTotals
+        __syncthreads();
+
+        // if valid element, place the element in proper destination address based on thread offset in warp, warp offset in block, and block offset in grid
+        if (pred) {
+            d_output[t_u + warpTotals[w_i] + d_BlocksOffset[blockIdx.x]] = idx;//d_input[idx];
+        }
+
+
+    }
+}
+
+
+// blockSize = n * warpSize
+int get_nonzero_indices(bool *d_input, int *d_output, int length, int blockSize) {
+    int numBlocks = divup(length, blockSize);
+    int *d_BlocksCount;
+    int *d_BlocksOffset;
+
+    cudaMalloc(&d_BlocksCount, sizeof(int) * numBlocks);
+    cudaMalloc(&d_BlocksOffset, sizeof(int) * numBlocks);
+    thrust::device_ptr<int> thrustPrt_bCount(d_BlocksCount);
+    thrust::device_ptr<int> thrustPrt_bOffset(d_BlocksOffset);
+
+
+    //phase 1: count number of valid elements in each thread block
+    computeBlockCounts<<<numBlocks, blockSize>>>(d_input, length, d_BlocksCount);
+
+//    //phase 2: compute exclusive prefix sum of valid block counts to get output offset for each thread block in grid
+    thrust::exclusive_scan(thrustPrt_bCount, thrustPrt_bCount + numBlocks, thrustPrt_bOffset);
+//
+//    //phase 3: compute output offset for each thread in warp and each warp in thread block, then output valid elements
+    compactK<<<numBlocks, blockSize, sizeof(int) * (blockSize / warpSize)>>>(d_input, length, d_output,
+                                                                             d_BlocksOffset);
+//
+//    // determine number of elements in the compacted list
+
+
+    int compact_length = thrustPrt_bOffset[numBlocks - 1] + thrustPrt_bCount[numBlocks - 1];
+
+    cudaFree(d_BlocksCount);
+    cudaFree(d_BlocksOffset);
+
+    return compact_length;
+}
+
+
+#endif /* CUCOMPACTOR_H_ */
---
+++ main.cu
@@ -0,0 +1,270 @@
+#include <cmath>
+#include <iostream>
+#include <thrust/scan.h>
+#include <thrust/device_vector.h>
+
+#include "PrefixSum.cuh"
+#include "StreamIndexCompaction.cuh"
+#include <cuda_runtime.h>
+#include <cublas_v2.h>
+
+const int NUM_THREADS = 1024;
+
+inline int getNumBlock(int n, int threads) {
+    return (n - 1) / threads + 1;
+}
+
+
+__global__ void
+frameDifferencing(const float *currImg, const float *prevImg, float *diffImg, bool *idMapImg, int chan, int bound) {
+    // img layout (H, W, C)
+    int offset = threadIdx.x + blockDim.x * blockIdx.x;
+    if (offset < bound) {
+
+        int offset_img = offset * chan;
+        bool is_id = true;
+
+        // consecutive access to the buffer... so I hope it utilizes the cache well.
+        for (int i = 0; i < chan; i++) {
+            int idx = i + offset_img;
+            float d = currImg[idx] - prevImg[idx];
+
+            diffImg[idx] = d;
+
+
+            // epsilon. may slightly differ from context to context.
+            if (d > 0.001) {
+                is_id = false;
+            }
+        }
+
+        idMapImg[offset] = is_id;
+    }
+}
+
+__global__ void convolveIdMap(bool *idMapImg, bool *idMapConvImg,
+                              int w, int h,
+                              int conv_w, int conv_h,
+                              int ker_w, int ker_h,
+                              int stride_x, int stride_y,
+                              int pad_x, int pad_y,
+                              int dilation_w, int dilation_h) {
+
+    int idx = threadIdx.x + blockDim.x * blockIdx.x;
+
+    if (idx < conv_w * conv_h) {
+
+        int x = idx % blockDim.x;
+        int y = idx / blockDim.x;
+
+        int offset_x = x * stride_x - pad_x;
+        int offset_y = y * stride_y - pad_y;
+
+        int count = 0;
+
+        // we may use some relaxed threshold other than 1, (e.g., conv_w * conv_h / 10)
+        int count_thres = 1;
+
+        bool exit_loop = false;
+
+        for (int i = 0; i < ker_h; ++i) {
+            int ker_y = i * dilation_h + offset_y;
+            for (int j = 0; j < ker_w; ++j) {
+                int ker_x = j * dilation_w + offset_x;
+                if (ker_y >= 0 && ker_x >= 0 && ker_y < h && ker_x < w) {
+                    if (idMapImg[ker_y * w + ker_x]) {
+                        count++;
+                        if (count >= count_thres) {
+                            exit_loop = true;
+                            break;
+                        }
+
+                    }
+                }
+            }
+            if (exit_loop) {
+                break;
+            }
+        }
+        idMapConvImg[y * conv_w + x] = count >= count_thres;
+    }
+}
+
+__global__ void im2scol(int *conv_indices, float *diffImg,
+                        float *scol_out,
+                        int num_chan,
+                        int w, int h,
+                        int conv_w, int conv_h,
+                        int ker_w, int ker_h,
+                        int stride_x, int stride_y,
+                        int pad_x, int pad_y,
+                        int dilation_w, int dilation_h) {
+
+    int idx = threadIdx.x + blockDim.x * blockIdx.x;
+    int chan_idx = idx % num_chan;
+
+    int t_idx = idx / num_chan;
+
+    if (t_idx < conv_w * conv_h) {
+
+        int conv_idx = conv_indices[t_idx];
+
+        int conv_x = conv_idx % conv_w;
+        int conv_y = conv_idx / conv_w;
+
+        int img_x_offset = conv_x * stride_x - pad_x;
+        int img_y_offset = conv_y * stride_y - pad_y;
+
+        int scol_idx_offset = t_idx * num_chan * ker_w * ker_h + chan_idx * (ker_w * ker_h);
+
+        for (int ker_y = 0; ker_y < ker_h; ++ker_y) {
+            int img_y = ker_y * dilation_h + img_y_offset;
+            for (int ker_x = 0; ker_x < ker_w; ++ker_x) {
+                int img_x = ker_x * dilation_w + img_x_offset;
+                if (img_y >= 0 && img_x >= 0 && img_y < h && img_x < w) {
+
+                    int img_idx = img_y * w * num_chan + img_x * num_chan + chan_idx;
+
+                    int scol_idx = scol_idx_offset + ker_y * ker_w + ker_x;
+
+                    scol_out[scol_idx] = diffImg[img_idx];
+                }
+            }
+        }
+
+
+    }
+
+
+}
+
+
+// Multiply the arrays A and B on GPU and save the result in C
+// C(m,n) = A(m,k) * B(k,n)
+void gemm(const float *A, const float *B, float *C, const int m, const int k, const int n) {
+    int lda = m, ldb = k, ldc = m;
+    const float alf = 1;
+    const float bet = 0;
+    const float *alpha = &alf;
+    const float *beta = &bet;
+
+    // Create a handle for CUBLAS
+    cublasHandle_t handle;
+    cublasCreate(&handle);
+
+    // Do the actual multiplication
+    cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc);
+
+    // Destroy the handle
+    cublasDestroy(handle);
+}
+
+int main() {
+
+    int imgInW = 1024, imgInH = 1024;
+    int imgInChan = 3;
+    int imgOutChan = 4;
+    int kerW = 8, kerH = 8;
+    int strideX = 1, strideY = 1;
+    int padX = 0, padY = 0;
+    int dilationX = 1, dilationY = 1;
+
+    int imgOutW = (imgInW + 2 * padX - dilationX * (kerW - 1) - 1) / strideX;
+    int imgOutH = (imgInH + 2 * padY - dilationY * (kerH - 1) - 1) / strideY;
+
+
+    float *prevImgIn, *prevImgOut;
+    float *currImgIn, *currImgOut;
+
+    cudaMalloc(&prevImgIn, imgInH * imgInW * imgInChan * sizeof(float));
+    cudaMalloc(&currImgIn, imgInH * imgInW * imgInChan * sizeof(float));
+    cudaMalloc(&prevImgOut, imgOutH * imgOutW * imgOutChan * sizeof(float));
+    cudaMalloc(&currImgOut, imgOutH * imgOutW * imgOutChan * sizeof(float));
+
+    // do some cudaMemcpy() stuff here...
+
+    // Define some convolution kernels here
+
+    float *convKernel;
+    cudaMalloc(&convKernel, imgInChan * kerH * kerW * imgOutChan * sizeof(float));
+
+
+
+    // STEP 1: Frame differencing
+
+    // diffImg (imgInH, imgInW, imgInChan)
+    // currImg - affineTransform(prevImg) 의 결과. 여기서 prevImg에 적용되는 아핀변환은 diffImg의 sparsity를 최대화할 수 있어야 함.
+
+    float *diffImg;
+    bool *idMapImg; // dim [H, W] | 모든 채널이 같으면 0 ('같다'의 기준을 조금 널널하게 할 수도 있을듯), 하나라도 다르면 1.
+
+    cudaMalloc(&diffImg, imgInH * imgInW * imgInChan * sizeof(float));
+    cudaMalloc(&idMapImg, imgInH * imgInW * sizeof(bool));
+
+    frameDifferencing<<<getNumBlock(imgInH * imgInW, NUM_THREADS), NUM_THREADS>>>(currImgIn, prevImgIn, diffImg,
+                                                                                  idMapImg,
+                                                                                  imgInChan, imgInH * imgInW);
+
+
+    // STEP 2: Apply max convolution to idMapImg
+
+    bool *idMapConvImg;
+    cudaMalloc(&idMapConvImg, imgOutH * imgOutW * sizeof(bool));
+
+
+    convolveIdMap<<<getNumBlock(imgOutH * imgOutW, NUM_THREADS), NUM_THREADS>>>(idMapImg, idMapConvImg, imgInW, imgInH,
+                                                                                imgOutW, imgOutH, kerW, kerH, strideX,
+                                                                                strideY,
+                                                                                padX, padY, dilationX, dilationY);
+
+
+    // STEP 3: Convert idMapConvImg into sparse form
+
+    int *indices;
+    cudaMalloc(&indices, imgOutH * imgOutW * sizeof(int));
+
+    int num_indices = get_nonzero_indices(idMapConvImg, indices, imgOutH * imgOutW, warpSize * 4);
+
+    printf("%i", num_indices);
+
+
+    // STEP 4: image to sparse column
+    float *s_col;
+    cudaMalloc(&s_col, num_indices * imgInChan * kerW * kerH * sizeof(float));
+
+    im2scol<<<getNumBlock(num_indices * imgInChan, NUM_THREADS), NUM_THREADS>>>(indices, diffImg,
+                                                                                s_col,
+                                                                                imgInChan,
+                                                                                imgInW, imgInH,
+                                                                                imgOutW, imgOutH, kerW, kerH, strideX,
+                                                                                strideY,
+                                                                                padX, padY, dilationX, dilationY);
+
+    // STEP 5: (Dense) GEMM
+
+    float *s_img;
+    cudaMalloc(&s_img, num_indices * imgOutChan * sizeof(float));
+
+    gemm(s_col, convKernel, s_img, num_indices, imgInChan * kerH * kerW, imgOutChan);
+
+
+
+    // STEP 6: sparse column to image
+
+
+
+    // Free memory
+    cudaFree(prevImgIn);
+    cudaFree(currImgIn);
+    cudaFree(prevImgOut);
+    cudaFree(currImgOut);
+    cudaFree(convKernel);
+
+
+    cudaFree(diffImg);
+    cudaFree(idMapImg);
+    cudaFree(idMapConvImg);
+    cudaFree(indices);
+
+    return 0;
+}
